bc3cde0f618944d07e05d4706360dc5f6f31ac06
Yicong Fu <fuyicong1996@gmail.com>
2024-02-07 15:10:26 -0500

    added cholesky solver and lapack header

--- include/defs.h
+++ include/defs.h
@@ -7,24 +7,29 @@
 
 namespace SparseUtils {
 
-/**
- * @brief The type for array indexing.
- *
- * Note: ptrdiff_t is preferable over int because int is often 32 bits and might
- * not be large enough for 64-bit environments. ptrdiff_t is also preferable
- * over size_t because it is signed.
- *
- * Caveat: If the array size is so large (greater than PTRDIFF_MAX but less than
- * SIZE_MAX), element indices may not be representable by ptrdiff_t and the
- * behavior is undefined. But usually ptrdiff_t is 64-bit which is sufficient.
- *
- * Refs:
- * - https://github.com/isocpp/CppCoreGuidelines/pull/1115
- * - https://stackoverflow.com/a/48730597
- * - https://en.cppreference.com/w/cpp/types/ptrdiff_t
- *
- */
-using index_t = std::ptrdiff_t;
+// /**
+//  * @brief The type for array indexing.
+//  *
+//  * Note: ptrdiff_t is preferable over int because int is often 32 bits and
+//  might
+//  * not be large enough for 64-bit environments. ptrdiff_t is also preferable
+//  * over size_t because it is signed.
+//  *
+//  * Caveat: If the array size is so large (greater than PTRDIFF_MAX but less
+//  than
+//  * SIZE_MAX), element indices may not be representable by ptrdiff_t and the
+//  * behavior is undefined. But usually ptrdiff_t is 64-bit which is
+//  sufficient.
+//  *
+//  * Refs:
+//  * - https://github.com/isocpp/CppCoreGuidelines/pull/1115
+//  * - https://stackoverflow.com/a/48730597
+//  * - https://en.cppreference.com/w/cpp/types/ptrdiff_t
+//  *
+//  */
+// using index_t = std::ptrdiff_t;
+
+using index_t = int;  // we use int for now because that's what lapack uses...
 
 template <typename T>
 using complex_t = std::complex<T>;
