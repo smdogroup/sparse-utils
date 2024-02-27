bec6008dcc07c1686c8401ddb0cfd619901dca69
Yicong Fu <fuyicong1996@gmail.com>
2024-02-27 10:31:28 -0500

    add functor versions of connectivity tools

--- include/sparse_utils/connectivity_tools.h
+++ include/sparse_utils/connectivity_tools.h
@@ -46,14 +46,60 @@ void NodeToElementFromConnectivity(int num_nodes, int num_elements,
   *node_to_elem_ = node_to_elem;
 }
 
-void CSRFromConnectivity(int num_nodes, int num_elements,
-                         const int nodes_per_element, const int* element_nodes,
-                         int** rowp_, int** cols_) {
+template <class Func>
+void NodeToElementFromConnectivityFunctor(int num_nodes, int num_elements,
+                                          const int nodes_per_element,
+                                          const Func& element_nodes_func,
+                                          int** node_to_elem_ptr_,
+                                          int** node_to_elem_) {
+  // Create data to store node -> element connectivity
+  int* node_to_elem_ptr = new int[num_nodes + 1];
+  for (int i = 0; i < num_nodes + 1; i++) {
+    node_to_elem_ptr[i] = 0;
+  }
+
+  int nodes[nodes_per_element];
+  for (int i = 0; i < num_elements; i++) {
+    element_nodes_func(i, nodes);
+    for (int j = 0; j < nodes_per_element; j++) {
+      node_to_elem_ptr[nodes[j] + 1]++;
+    }
+  }
+
+  for (int i = 0; i < num_nodes; i++) {
+    node_to_elem_ptr[i + 1] += node_to_elem_ptr[i];
+  }
+
+  // Set up the node to element data
+  int* node_to_elem = new int[node_to_elem_ptr[num_nodes]];
+  for (int i = 0; i < num_elements; i++) {
+    element_nodes_func(i, nodes);
+    for (int j = 0; j < nodes_per_element; j++) {
+      int node = nodes[j];
+      node_to_elem[node_to_elem_ptr[node]] = i;
+      node_to_elem_ptr[node]++;
+    }
+  }
+
+  for (int i = num_nodes; i > 0; i--) {
+    node_to_elem_ptr[i] = node_to_elem_ptr[i - 1];
+  }
+  node_to_elem_ptr[0] = 0;
+
+  // Set the outputs
+  *node_to_elem_ptr_ = node_to_elem_ptr;
+  *node_to_elem_ = node_to_elem;
+}
+
+void CSRFromConnectivityFunctor(int num_nodes, int num_elements,
+                                const int nodes_per_element,
+                                const int* element_nodes, int** rowp_,
+                                int** cols_) {
   int* node_to_elem_ptr = nullptr;
   int* node_to_elem = nullptr;
-  NodeToElementFromConnectivity(num_nodes, num_elements, nodes_per_element,
-                                element_nodes, &node_to_elem_ptr,
-                                &node_to_elem);
+  NodeToElementFromConnectivityFunctor(num_nodes, num_elements,
+                                       nodes_per_element, element_nodes,
+                                       &node_to_elem_ptr, &node_to_elem);
 
   // Set up the CSR data structure
   int* rowp = new int[num_nodes + 1];
@@ -94,6 +140,77 @@ void CSRFromConnectivity(int num_nodes, int num_elements,
       int elem = node_to_elem[j];
       for (int k = 0; k < nodes_per_element; k++) {
         int node = element_nodes[elem * nodes_per_element + k];
+        if (counter[node] < i) {
+          counter[node] = i;
+          cols[nnz] = node;
+          nnz++;
+        }
+      }
+    }
+  }
+
+  *rowp_ = rowp;
+  *cols_ = cols;
+
+  // Free unused data
+  delete[] node_to_elem_ptr;
+  delete[] node_to_elem;
+  delete[] counter;
+}
+
+template <class Func>
+void CSRFromConnectivity(int num_nodes, int num_elements,
+                         const int nodes_per_element,
+                         const Func& element_nodes_func, int** rowp_,
+                         int** cols_) {
+  int* node_to_elem_ptr = nullptr;
+  int* node_to_elem = nullptr;
+  NodeToElementFromConnectivity(num_nodes, num_elements, nodes_per_element,
+                                element_nodes_func, &node_to_elem_ptr,
+                                &node_to_elem);
+
+  // Set up the CSR data structure
+  int* rowp = new int[num_nodes + 1];
+  int* counter = new int[num_nodes];
+
+  // Initialize the counter
+  for (int i = 0; i < num_nodes; i++) {
+    counter[i] = -1;
+  }
+
+  // Count up the number of non-zero entries
+  rowp[0] = 0;
+  int nodes[nodes_per_element];
+  for (int i = 0, nnz = 0; i < num_nodes; i++) {
+    for (int j = node_to_elem_ptr[i]; j < node_to_elem_ptr[i + 1]; j++) {
+      int elem = node_to_elem[j];
+      element_nodes_func(elem, nodes);
+      for (int k = 0; k < nodes_per_element; k++) {
+        int node = nodes[k];
+        if (counter[node] < i) {
+          counter[node] = i;
+          nnz++;
+        }
+      }
+    }
+    rowp[i + 1] = nnz;
+  }
+
+  // Allocate the column indices
+  int* cols = new int[rowp[num_nodes]];
+
+  // Reset the counter
+  for (int i = 0; i < num_nodes; i++) {
+    counter[i] = -1;
+  }
+
+  // Count up the number of non-zero entries
+  for (int i = 0, nnz = 0; i < num_nodes; i++) {
+    for (int j = node_to_elem_ptr[i]; j < node_to_elem_ptr[i + 1]; j++) {
+      int elem = node_to_elem[j];
+      element_nodes_func(elem, nodes);
+      for (int k = 0; k < nodes_per_element; k++) {
+        int node = nodes[k];
         if (counter[node] < i) {
           counter[node] = i;
           cols[nnz] = node;
